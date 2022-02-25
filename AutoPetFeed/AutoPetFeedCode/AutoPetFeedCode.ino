44f98b7a2537197ead0fc0d363a5df694359975e
Johnny <Apollo0611@icloud.com>
2022-02-25 16:02:48 +0900

    Release Candidate 1 [AutoPetFeedCode.ino]

--- AutoPetFeed/AutoPetFeedCode/AutoPetFeedCode.ino
+++ AutoPetFeed/AutoPetFeedCode/AutoPetFeedCode.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 #include <RTClib_Johnny.h>
-#include "HX711_ASW.h"
+#include "HX711.h"
 #include "settings.h"
 
 Hx711 scale(A1, A0);
@@ -9,21 +9,10 @@ Servo servo1;
 Servo servo2;
 Servo servo3;
 
-void showWeight()
-{
-  Serial.print(scale.gram() * 2, 1);
-  Serial.println(" g");
-}
 
 void rtcCalibration()
 {
   rtc.adjust(DateTime(__DATE__, __TIME__));
-}
-
-void showDate()
-{
-  DateTime now = rtc.now();
-  Serial.println(now.tostr(buf));
 }
 
 void servoCalibration()
@@ -34,6 +23,27 @@ void servoCalibration()
   servo1.write(servoStop);
   servo2.write(servoStop);
   servo3.write(servoStop);
+}
+
+void debugLog()
+{
+  DateTime now = rtc.now();
+  Serial.print(now.tostr(buf));
+  Serial.print(" | wheight : ");
+  Serial.print(scale.gram() * 2, 1);
+  Serial.println(" g");
+}
+
+void showWeight()
+{
+  Serial.print(scale.gram() * 2, 1);
+  Serial.println(" g");
+}
+
+void showDate()
+{
+  DateTime now = rtc.now();
+  Serial.println(now.tostr(buf));
 }
 
 void servo1Run()
@@ -91,78 +101,108 @@ void servo3Run()
   servo3.detach();
 }
 
-void bluetoothRecieve()
+void servo1Operating()
 {
-  String buffer = Serial1.readStringUntil('\n');
+  DateTime now = rtc.now();
 
-  int firstSeparator = buffer.indexOf("/", 0);
-  int secondSeparator = buffer.indexOf("/", (firstSeparator + 1));
-  int lengthOfBuffer = buffer.length();
-
-  int modeBuffer = buffer.substring(0, firstSeparator).toInt();
-
-  switch (modeBuffer)
+  if((now.hour() == servo1BreakfastHour && now.minute() == servo1BreakfastMinute) ||
+     (now.hour() == servo1LunchHour && now.minute() == servo1LunchMinute) ||
+     (now.hour() == servo1DinnerHour && now.minute() == servo1DinnerMinute))
   {
-  case 1:
-    servo1Gram = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    break;
+    servo1ReserveState = true;
+    servo1ResetTime = now.minute() + 5;
 
-  case 11:
-    servo1BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo1BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+    if(servo1ResetTime >= 60)
+      servo1ResetTime -= 60;
+  }
 
-  case 12:
-    servo1LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo1LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+  if (servo1ReserveState == true && servo1RunState == false)
+  {
+    servo1Run();
+    servo1RunState = true;
+    
+    Serial.println("servo1 is running now");
+    Serial.print("servo1 ResetTime is ");
+    Serial.println(servo1ResetTime);
+  }
 
-  case 13:
-    servo1DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo1DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+  if(now.minute() == servo1ResetTime)
+  {
+    if(servo1RunState)
+      Serial.println("servo1 Reset!");
 
-  case 2:
-    servo2Count = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    break;
+    servo1ReserveState = false;
+    servo1RunState = false;
+  }
+}
 
-  case 21:
-    servo2BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo2BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+void servo2Operating()
+{
+  DateTime now = rtc.now();
 
-  case 22:
-    servo2LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo2LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+  if((now.hour() == servo2BreakfastHour && now.minute() == servo2BreakfastMinute) ||
+     (now.hour() == servo2LunchHour && now.minute() == servo2LunchMinute) ||
+     (now.hour() == servo2DinnerHour && now.minute() == servo2DinnerMinute))
+  {
+    servo2ReserveState = true;
+    servo2ResetTime = now.minute() + 5;
 
-  case 23:
-    servo2DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo2DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+       if(servo2ResetTime >= 60)
+        servo2ResetTime -= 60;
+  }
 
-  case 3:
-    servo3Count = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    break;
+  if (servo2ReserveState == true && servo2RunState == false)
+  {
+    servo2Run();
+    servo2RunState = true;
 
-  case 31:
-    servo3BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo3BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+    Serial.println("servo2 is running now");
+    Serial.print("servo2 ResetTime is ");
+    Serial.println(servo2ResetTime);
+  }
 
-  case 32:
-    servo3LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo3LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+  if(now.minute() == servo2ResetTime)
+  {
+    if(servo2RunState)
+      Serial.println("servo2 Reset!");
 
-  case 33:
-    servo3DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
-    servo3DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
-    break;
+    servo2ReserveState = false;
+    servo2RunState = false;
+  }
+}
 
-  default:
-    //nothing
-    break;
+void servo3Operating()
+{
+  DateTime now = rtc.now();
+
+  if((now.hour() == servo3BreakfastHour && now.minute() == servo3BreakfastMinute) ||
+     (now.hour() == servo3LunchHour && now.minute() == servo3LunchMinute) ||
+     (now.hour() == servo3DinnerHour && now.minute() == servo3DinnerMinute))
+  {
+    servo3ReserveState = true;
+    servo3ResetTime = now.minute() + 5;
+
+       if(servo3ResetTime >= 60)
+      servo3ResetTime -= 60;
+  }
+
+  if (servo3ReserveState == true && servo3RunState == false)
+  {
+    servo3Run();
+    servo3RunState = true;
+
+    Serial.println("servo3 is running now");
+    Serial.print("servo3 ResetTime is ");
+    Serial.println(servo3ResetTime);
+  }
+
+  if(now.minute() == servo3ResetTime)
+  {
+    if(servo3RunState)
+      Serial.println("servo3 Reset!");
+
+    servo3ReserveState = false;
+    servo3RunState = false;
   }
 }
 
@@ -178,10 +218,134 @@ void buzzerAlarm(int num = 1)
   delay(500);
 }
 
-void debug()
+void bluetoothRecieve()
 {
-  showWeight();
-  showDate();
+  String buffer = Serial1.readStringUntil('\n');
+
+  int firstSeparator = buffer.indexOf("/", 0);
+  int secondSeparator = buffer.indexOf("/", (firstSeparator + 1));
+  int lengthOfBuffer = buffer.length();
+
+  int modeBuffer = buffer.substring(0, firstSeparator).toInt();
+
+  switch (modeBuffer)
+  {
+  case 1:
+    servo1Gram = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    
+    Serial.print("servo1Gram - ");
+    Serial.println(servo1Gram);
+    break;
+
+  case 11:
+    servo1BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo1BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+    
+    Serial.print("servo1Breakfast - ");
+    Serial.print(servo1BreakfastHour);
+    Serial.print(" : ");
+    Serial.println(servo1BreakfastMinute);
+    break;
+
+  case 12:
+    servo1LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo1LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo1Lunch - ");
+    Serial.print(servo1LunchHour);
+    Serial.print(" : ");
+    Serial.println(servo1LunchMinute);
+    break;
+
+  case 13:
+    servo1DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo1DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo1Dinner - ");
+    Serial.print(servo1DinnerHour);
+    Serial.print(" : ");
+    Serial.println(servo1DinnerMinute);
+    break;
+
+  case 2:
+    servo2Count = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+
+    Serial.print("servo2Count - ");
+    Serial.println(servo2Count);
+    break;
+
+  case 21:
+    servo2BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo2BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo2Breakfast - ");
+    Serial.print(servo2BreakfastHour);
+    Serial.print(" : ");
+    Serial.println(servo2BreakfastMinute);
+    break;
+
+  case 22:
+    servo2LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo2LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo2Lunch - ");
+    Serial.print(servo2LunchHour);
+    Serial.print(" : ");
+    Serial.println(servo2LunchMinute);
+    break;
+
+  case 23:
+    servo2DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo2DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo2Dinner - ");
+    Serial.print(servo2DinnerHour);
+    Serial.print(" : ");
+    Serial.println(servo2DinnerMinute);
+    break;
+
+  case 3:
+    servo3Count = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+
+    Serial.print("servo3Count - ");
+    Serial.println(servo3Count);
+    break;
+
+  case 31:
+    servo3BreakfastHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo3BreakfastMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo3Breakfast - ");
+    Serial.print(servo3BreakfastHour);
+    Serial.print(" : ");
+    Serial.println(servo3BreakfastMinute);
+    break;
+
+  case 32:
+    servo3LunchHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo3LunchMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo3Lunch - ");
+    Serial.print(servo3LunchHour);
+    Serial.print(" : ");
+    Serial.println(servo3LunchMinute);
+    break;
+
+  case 33:
+    servo3DinnerHour = buffer.substring(firstSeparator + 1, secondSeparator).toInt();
+    servo3DinnerMinute = buffer.substring(secondSeparator + 1, lengthOfBuffer).toInt();
+
+    Serial.print("servo3Dinner - ");
+    Serial.print(servo3DinnerHour);
+    Serial.print(" : ");
+    Serial.println(servo3DinnerMinute);
+    break;
+
+  default:
+    Serial.print("Wrong Input : ");
+    Serial.println(Serial1.read());
+    break;
+  }
 }
 
 void setup()
@@ -192,6 +356,8 @@ void setup()
   rtc.begin();
 
   pinMode(buzzerPin, OUTPUT);
+
+  rtcCalibration();
 }
 
 void loop()
@@ -199,6 +365,11 @@ void loop()
   if (Serial1.available())
     bluetoothRecieve();
 
-  debug();
-  delay(100);
+  servo1Operating();
+  servo2Operating();
+  servo3Operating();
+
+  debugLog();
+
+  delay(5000);
 }
--- AutoPetFeed/AutoPetFeedCode/settings.h
+++ AutoPetFeed/AutoPetFeedCode/settings.h
@@ -1,9 +1,21 @@
-const int servo1Pin = 8;
-const int servo2Pin = 9;
-const int servo3Pin = 10;
-const int buzzerPin = 12;
+//디버깅 모드 20씩 빼세요
+const int servo1Pin = 28; 
+const int servo2Pin = 29;
+const int servo3Pin = 30;
+const int buzzerPin = 32;
 
 char buf[20];
+
+bool servo1ReserveState = false;
+bool servo1RunState = false;
+bool servo2ReserveState = false;
+bool servo2RunState = false;
+bool servo3ReserveState = false;
+bool servo3RunState = false;
+
+int servo1ResetTime = 255;
+int servo2ResetTime = 255;
+int servo3ResetTime = 255;
 
 const int angle225Delay = 3000;
 const int angle450Delay = 4500;
@@ -12,20 +24,25 @@ const int servoRun = 80;
 const int servoStop = 90;
 
 int servo1Gram = 100;
+
 int servo1BreakfastHour = 255;
 int servo1BreakfastMinute = 255;
 int servo1LunchHour = 255;
 int servo1LunchMinute = 255;
 int servo1DinnerHour = 255;
 int servo1DinnerMinute = 255;
+
 int servo2Count = 1;
+
 int servo2BreakfastHour = 255;
 int servo2BreakfastMinute = 255;
 int servo2LunchHour = 255;
 int servo2LunchMinute = 255;
 int servo2DinnerHour = 255;
 int servo2DinnerMinute = 255;
+
 int servo3Count = 1;
+
 int servo3BreakfastHour = 255;
 int servo3BreakfastMinute = 255;
 int servo3LunchHour = 255;
